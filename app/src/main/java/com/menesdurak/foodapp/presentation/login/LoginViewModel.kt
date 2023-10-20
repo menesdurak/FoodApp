42bcae3ae06f4752e7711ae6e496b8426e64d444
menesdurak <m.enes.durak@gmail.com>
2023-10-20 08:14:10 +0300

    Removed unnecessary auth use cases

--- app/src/main/java/com/menesdurak/foodapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/menesdurak/foodapp/presentation/login/LoginViewModel.kt
@@ -1,34 +1,28 @@
 package com.menesdurak.foodapp.presentation.login
 
 import androidx.lifecycle.ViewModel
-import com.menesdurak.foodapp.domain.usecase.GetUserMailUseCase
-import com.menesdurak.foodapp.domain.usecase.SignInUseCase
-import com.menesdurak.foodapp.domain.usecase.SignOutUseCase
-import com.menesdurak.foodapp.domain.usecase.SignUpUseCase
+import com.menesdurak.foodapp.domain.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val signInUseCase: SignInUseCase,
-    private val signOutUseCase: SignOutUseCase,
-    private val signUpUseCase: SignUpUseCase,
-    private val getUserMailUseCase: GetUserMailUseCase
+    private val authRepository: AuthRepository
 ): ViewModel() {
 
     fun signUp(email: String, password: String): String {
-        return signUpUseCase.invoke(email, password)
+        return authRepository.signUp(email, password)
     }
 
     fun signIn(email: String, password: String): String {
-        return signInUseCase.invoke(email, password)
+        return authRepository.signIn(email, password)
     }
 
     fun signOut() {
-        signOutUseCase.invoke()
+        authRepository.signOut()
     }
 
     fun getUserMail(): String {
-        return getUserMailUseCase.invoke()
+        return authRepository.getUserMail()
     }
 }
